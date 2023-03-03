939a4d142c3517da6d40e0a6659ab569091942cd
g.pan <pangleb1@gmail.com>
2023-03-03 17:36:32 +0500

    new project has been created

---
+++ Valve_Jacket_Generator/Calculation/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+
+class ValveParams(models.Model):
+    flange_c = models.CharField(max_length=10)
+    flange_thck = models.CharField(max_length=10)
+    shield_c = models.CharField(max_length=10)
+    pipe_c = models.CharField(max_length=10)
+    valve_c = models.CharField(max_length=10)
+    valve_l = models.CharField(max_length=10)
+    gap_left = models.CharField(max_length=10)
+    gap_right = models.CharField(max_length=10)
